2e981dbff80ed2eadd61191d7dbe5a3452542974
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2018-07-02 14:12:57 +0300

    Create Solution Projects

---
+++ MyInterviewQuestions/MyInterviewQuestions.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyInterviewQuestions.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MyInterviewQuestions/MyInterviewQuestions.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyInterviewQuestions.WebApi.WebApiApplication" Language="C#" %>
